8d912214a6204b6a0b21314aa4d8c00296792527
Xzese <applesam@hotmail.co.uk>
2024-05-31 21:52:54 +0100

    Added code the setup and initiate the

--- plex_sonarr_automation.py
+++ plex_sonarr_automation.py
@@ -23,15 +23,46 @@ def get_env_variable(var_name, prompt):
         set_key(str(env_path), var_name, value)
     return value
 
+# Function to validate and prompt for a positive integer input
+def get_non_negative_integer(prompt):
+    while True:
+        try:
+            value = int(input(f"{prompt}: "))
+            if value < 0:
+                print("Please enter a non negative integer.")
+            else:
+                return value
+        except ValueError:
+            print("Please enter a valid non negative integer.")
+
 # Check and prompt for necessary environment variables
 plex_url = get_env_variable('PLEX_URL', 'Please enter your Plex URL')
 plex_token = get_env_variable('PLEX_TOKEN', 'Please enter your Plex Token')
 sonarr_url = get_env_variable('SONARR_URL', 'Please enter your Sonarr URL')
 sonarr_key = get_env_variable('SONARR_KEY', 'Please enter your Sonarr API Key')
-days_until_deletion = get_env_variable('DAYS_TO_DELETE', 'Please enter the number of days until deletion')
+
+# Validate and prompt for the number of days until deletion
+days_until_deletion = os.getenv('DAYS_TO_DELETE')
+if days_until_deletion:
+    while True:
+        try:
+            days_until_deletion = int(days_until_deletion)
+            if days_until_deletion < 0:
+                print("DAYS_TO_DELETE must be a positive integer.")
+                days_until_deletion = get_non_negative_integer('Please enter the number of days until deletion')
+            else:
+                break
+        except ValueError:
+            print("DAYS_TO_DELETE must be a positive integer.")
+            days_until_deletion = get_non_negative_integer('Please enter the number of days until deletion')
+else:
+    days_until_deletion = get_non_negative_integer('Please enter the number of days until deletion')
+
+# Set the DAYS_TO_DELETE environment variable in the .env file
+set_key(str(env_path), 'DAYS_TO_DELETE', str(days_until_deletion))
 
 # Add 'd' to days_until_deletion
-days_until_deletion = days_until_deletion + "d"
+days_until_deletion = str(days_until_deletion) + "d"
 
 # Set up Plex and Sonarr instances
 plex = PlexServer(plex_url, plex_token)
@@ -39,11 +70,8 @@ showLibrary = plex.library.section('TV Shows')
 sonarr = SonarrAPI(sonarr_url, sonarr_key)
 payload = {"monitored": False}
 
-sys.exit()
-#setting variable = "2d" will mean all completed episodes that have a last viewed date more than 2 days ago will be deleted
-days_until_deletion = "2d"
 episode_dict = {}
-
+print(plex.library.sections())
 #Get All Unwatched Episodes over 2 days old and add to an nested dictionary in format {Show:[Episodes]}
 for episode in showLibrary.search(unwatched=False,libtype='episode',filters={"lastViewedAt<<":days_until_deletion}):
     for guid in episode.season().show().guids:
@@ -55,6 +83,8 @@ for episode in showLibrary.search(unwatched=False,libtype='episode',filters={"la
         if 'tvdb' in str(guid):
             episode_dict[tvShowKey].append(int(str(guid)[13:-1]))
 
+print(episode_dict)
+sys.exit()
 #Unmonitor and Delete all old watched episodes
 
 for tvshow_id, episode_ids in episode_dict.items():
